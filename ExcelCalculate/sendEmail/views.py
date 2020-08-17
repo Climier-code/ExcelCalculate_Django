224ea4526450b0b9ef4560317a734756a9135681
Climier-code <whddus0789@gmail.com>
2020-08-17 17:05:36 +0900

    2020_08_17

--- ExcelCalculate/sendEmail/views.py
+++ ExcelCalculate/sendEmail/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.mail import send_mail, EmailMessage
+from django.template.loader import render_to_string
+
+
+def send(reveiverEmail, verifyCode):
+    try:
+        content = {'verifyCode': verifyCode}
+        msg_html = render_to_string('sendEmail/email_format.html', content)
+        msg = EmailMessage(subject="인증 코드 발송 메일", body=msg_html, from_email="whddus0789@gmail.com", bcc=[reveiverEmail])
+        msg.content_subtype = 'html'
+        msg.send()
+        return True
+
+    except:
+        return False
+
 
 # Create your views here.
